4f71ca867f61456aa508d8721a76e048278a3045
apoorvaron <apoorv.aron02@gmail.com>
2022-07-23 15:42:05 +0530

    added admin folder

---
+++ env.php
@@ -0,0 +1,18 @@
+<!-- FOR Windows with XAMPP -->
+<?php
+        //   $env_server = "localhost:3306";
+        //   $env_username = "root";
+        //   $env_password = "";
+        //   $env_database = "tiny";
+        //   $env_port = "3306";
+?>
+
+
+<!-- for MAC with MAMP -->
+<?php
+    $env_server = "localhost";
+    $env_username = "root";
+    $env_password = "root";
+    $env_database = "acmbackend";
+    $env_port = "8889";
+?>
