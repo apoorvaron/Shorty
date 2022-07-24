2f77db51cfcce8e54f4026ecb7d2eeefd165d1a0
apoorvaron <apoorv.aron02@gmail.com>
2022-07-24 18:30:45 +0530

    main page changed

--- index.php
+++ index.php
@@ -3,60 +3,74 @@
   <head>  
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+<link rel="stylesheet" href="./assets/css/main.css">
   </head>  
   <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   <body>
-  <section class="vh-100" style="background-color: #9A616D;">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col col-xl-10">
-        <div class="card" style="border-radius: 1rem;">
-          <div class="row g-0">
-            <div class="col-md-6 col-lg-5 d-none d-md-block">
-              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
-                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+ <!-- Section: Design Block -->
+<section class="text-center">
+  <!-- Background image -->
+  <div class="p-5 bg-image" style="
+        height: 150px;
+        "></div>
+  <!-- Background image -->
+
+  <div class="card mx-4 mx-md-5 shadow-5-strong" style="
+        margin-top: -100px;
+        background: hsla(0, 0%, 100%, 0.8);
+        backdrop-filter: blur(30px);
+  ">
+    <div class="card-body py-5 px-md-5">
+
+      <div class="row d-flex justify-content-center">
+        <div class="col-lg-8">
+          <h2 class="fw-bold mb-5">Link Shortener</h2>
+
+          <form>
+
+            <div class="form-outline mb-4">
+              <input type="text" id="form3Example1" class="form-control"  placeholder="Link is for"/>
             </div>
-            <div class="col-md-6 col-lg-7 d-flex align-items-center">
-              <div class="card-body p-4 p-lg-5 text-black">
-
-                <form>
-
-                  <div class="d-flex align-items-center mb-3 pb-1">
-                    <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
-                    <span class="h1 fw-bold mb-0">Logo</span>
-                  </div>
-
-                  <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
-
-                  <div class="form-outline mb-4">
-                    <input type="email" id="form2Example17" class="form-control form-control-lg" />
-                    <label class="form-label" for="form2Example17">Email address</label>
-                  </div>
-
-                  <div class="form-outline mb-4">
-                    <input type="password" id="form2Example27" class="form-control form-control-lg" />
-                    <label class="form-label" for="form2Example27">Password</label>
-                  </div>
-
-                  <div class="pt-1 mb-4">
-                    <button class="btn btn-dark btn-lg btn-block" type="button">Login</button>
-                  </div>
-
-                  <a class="small text-muted" href="#!">Forgot password?</a>
-                  <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="#!"
-                      style="color: #393f81;">Register here</a></p>
-                  <a href="#!" class="small text-muted">Terms of use.</a>
-                  <a href="#!" class="small text-muted">Privacy policy</a>
-                </form>
-
-              </div>
+            <div class="form-outline mb-4">
+              <input type="text" id="form3Example4" class="form-control" placeholder="Original Link"/>
             </div>
-          </div>
+            <div class="form-outline mb-4 form-control">
+              <span>
+              <label> <b>http://micro.epizy.com/ </b> </label>
+              <input type="text" id="form3Example4"   placeholder="Custom Short Link" style="border:0px"/>
+              </span>
+            </div>
+      
+
+          
+
+              
+            <button type="submit" class="btn btn-primary btn-block mb-4">
+                Generate Random Name
+            </button>
+            &nbsp;&nbsp;
+
+            <button type="submit" class="btn btn-success btn-block mb-4">
+                Check & Confirm
+            </button>
+            <br>
+            <br>
+            <br>
+            <br>
+            <div class="d-flex align-items-center justify-content-center pb-4">
+                    <p class="mb-0 me-2">      Login As Admin</p>
+                    <button type="button" class="btn btn-outline-danger">Click Here</button>
+            </div>
+
+           
+          </form>
         </div>
       </div>
     </div>
   </div>
 </section>
+<!-- Section: Design Block -->
   </body>                
 </html>
