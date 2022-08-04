a1a74d304e8a707e6d4199d893a338ecf251bfb1
apoorvaron <apoorv.aron02@gmail.com>
2022-08-04 23:45:39 +0530

    logo removed

--- user/header.php
+++ user/header.php
@@ -101,18 +101,18 @@
 
 </style>
 <?php
-        include_once '../admin/dBconn/database.php';
-        $database = new Database();
-        $db = $database->connect();
+        // include_once '../admin/dBconn/database.php';
+        // $database = new Database();
+        // $db = $database->connect();
 
-        $username = $_GET['username'];
-        $uniqueNo = $_GET['uno'];
+        // $username = $_GET['username'];
+        // $uniqueNo = $_GET['uno'];
 
-        $sql = "SELECT * from users WHERE username='".$username."'&uniqueNo='".$uniqueNo."'";
-        $result = mysqli_query($db,$sql);
+        // $sql = "SELECT * from users WHERE username='".$username."'&uniqueNo='".$uniqueNo."'";
+        // $result = mysqli_query($db,$sql);
 
-        $row = mysqli_fetch_array($result);
-        $imgPath = $row['img'];
+        // $row = mysqli_fetch_array($result);
+        // $imgPath = $row['img'];
     
 ?>
 
@@ -126,7 +126,7 @@
                 <div class="topbar-left">
                     <div class="text-center">
                         <!--<a href="index.html" class="logo"><i class="fa fa-paw"></i> Aplomb</a>-->
-                        <a href="index.php?username=<?php echo $username ?>&uno=<?php echo $uno?>" class="logo"><img style="margin-top: 10%;" src="<?php echo $imgPath ;?>" height="100" alt="logo"></a>
+                        <a href="index.php?username=<?php echo $username ?>&uno=<?php echo $uno?>" class="logo"><img style="margin-top: 10%;" src="" height="100" alt="logo"></a>
                     </div>
                 </div>
                
