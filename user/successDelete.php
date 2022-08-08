37d98fdd246f6da801674edf5ac3a5611ad3f120
apoorvaron <apoorv.aron02@gmail.com>
2022-08-08 10:20:09 +0530

    ui fixed

--- user/successDelete.php
+++ user/successDelete.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-        <title>Make New Link</title>
+        <title>Delete Link</title>
         <meta content="Admin Dashboard" name="description" />
         <meta content="themesdesign" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
