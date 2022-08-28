71798ae565c4dadd68d0b5b3b1e814bb7889ac15
apoorvaron <apoorv.aron02@gmail.com>
2022-08-28 19:52:53 +0530

    dynamic year

--- user/footer.php
+++ user/footer.php
@@ -1,3 +1,7 @@
+
+<script>
+      document.getElementById("year").innerHTML = new Date().getFullYear();
+</script>
 <footer class="footer">
-      © 2022 -  All rights reserved. Design & Developed by <a target="_blank" href="https://github.com/apoorvaron" style="color:navy;">IPians</a>
+      © 2022 <span id="year"></span> -  All rights reserved. Design & Developed with ❤️ by <a target="_blank" href="https://github.com/apoorvaron" style="color:navy;">Apoorv Aron</a>
 </footer>
