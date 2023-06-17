af36b88ca24590dea74a8e8c081cb2c4328ed214
apoorvaron <apoorv.aron02@gmail.com>
2023-06-17 11:29:16 +0530

    3 files revert back

--- env.php
+++ env.php
@@ -1,18 +1,18 @@
-
+<!-- FOR XAMPP -->
 <?php
-$env_server = "localhost:3306";
-$env_username = "root";
-$env_password = "";
-$env_database = "tiny";
-$env_port = "3306";
+// $env_server = "localhost:3306";
+// $env_username = "root";
+// $env_password = "";
+// $env_database = "tiny";
+// $env_port = "3306";
 ?>
 
 
-
+<!-- FOR MAMP -->
 <?php
-// $env_server = "localhost";
-// $env_username = "root";
-// $env_password = "root";
-// $env_database = "tiny";
-// $env_port = "8889";
+$env_server = "localhost";
+$env_username = "root";
+$env_password = "root";
+$env_database = "tiny";
+$env_port = "8889";
 ?>
