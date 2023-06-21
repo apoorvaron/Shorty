355e4e5accfb0addc5a45544b9da39665d8a2d5a
PRAYAG PRAMOD BHOSALE <99162236+Mrprayag077@users.noreply.github.com>
2023-06-21 16:13:10 +0530

    seo tags faq.php

--- faq.php
+++ faq.php
@@ -9,45 +9,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
-    <title>Shorty</title>
-    <meta content="" name="description" />
-    <meta content="" name="keywords" />
+<!-- importing the SEO FROM SEO.PHP FILE (mrprayag) -->
+<?php include './includes/seo.php'; ?>
 
-
-  <title>Shorty | Powerful URL Shortener for Personalized, Shareable</title>
-
-  <!-- SEO SETTINGS (mrprayag) -->
-  <meta name="robots" content="index, follow">
-  <meta name="language" content="en">
-  <meta name="revisit-after" content="7 days">
-  <meta name="distribution" content="global">
-  <meta name="geo.region" content="india">
-  <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
-  <meta http-equiv="content-language" content="en">
-  <meta name="description"
-    content="Shorty is a powerful URL shortener that allows you to personalize, share, and track your content links, capturing valuable data with every click">
-  <meta name="keywords"
-    content="URL shortener, personalized links, shareable links, trackable links, link management,    Customized links,    Click data,    Link optimization,Link branding,    URL redirection,    Link performance,Link metrics,    URL management">
-
-  <!-- LINKS SEO (mrprayag077) -->
-  <link rel="canonical" href="http://shrty.rf.gd/">
-  <meta name="og:description"
-    content="Shorty is a powerful URL shortener that allows you to personalize, share, and track your content links, capturing valuable data with every click">
-
-
-  <!-- SEO for social-media-links sharing (mrprayag077) -->
-  <meta name="og:title" content="Shorty | Powerful URL Shortener for Personalized, Shareable, and Trackable Links">
-  <meta name="twitter:card" content="http://shrty.rf.gd/assets/img/inside-header-logo.png">
-  <meta name="twitter:site" content="@AronApoorv">
-  <meta name="twitter:image" content="http://shrty.rf.gd/assets/img/inside-header-logo.png">
-
-  <!-- <meta content="" name="" /> -->
-  <meta content="URL shortener, personalized links, trackable links" name="keywords" />
-
-  <!-- Favicons -->
-  <link href="./assets/img/logo.png" rel="icon" />
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
-
+    
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
