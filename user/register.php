eadfb12c1c38d358a4e39799fce8440248a41ffd
Bhavya Prasad <125134136+bhavya1857@users.noreply.github.com>
2023-06-27 13:30:20 +0530

    Update register.php

--- user/register.php
+++ user/register.php
@@ -203,7 +203,61 @@
                                     </div>
                                 </div>
                             </div> -->
+ <?php
 
+session_start();
+$msg = '';
+
+// If user has given a captcha!
+if (isset($_POST['input']) && sizeof($_POST['input']) > 0)
+
+	// If the captcha is valid
+	if ($_POST['input'] == $_SESSION['captcha'])
+		$msg = '<span style="color:green">SUCCESSFUL!!!</span>';
+	else
+		$msg = '<span style="color:red">CAPTCHA FAILED!!!</span>';
+?>
+
+<style>
+	body{
+		display:flex;
+		flex-direction:column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
+
+<body>
+	<h2>PROVE THAT YOU ARE NOT A ROBOT!!</h2>
+	
+	<strong>
+		Type the text in the image to prove
+		you are not a robot
+	</strong>
+
+	<div style='margin:15px'>
+		<img src="captcha.php">
+	</div>
+	
+	<form method="POST" action=
+			" <?php echo $_SERVER['PHP_SELF']; ?>">
+		<input type="text" name="input"/>
+		<input type="hidden" name="flag" value="1"/>
+		<input type="submit" value="Submit" name="submit"/>
+	</form>
+	
+	<div style='margin-bottom:5px'>
+		<?php echo $msg; ?>
+	</div>
+	
+	<div>
+		Can't read the image? Click
+		<a href='<?php echo $_SERVER['PHP_SELF']; ?>'>
+			here
+		</a>
+		to refresh!
+	</div>
+</body>
                         <div class="form-group text-center row m-t-20">
                             <div class="col-12">
                                 <button class="btn  btn-block waves-effect waves-light" style="background-color: #0d42ff; color:white; border: 1px solid #0d42ff;" id="submit" name="submit" type="submit">Register</button>
