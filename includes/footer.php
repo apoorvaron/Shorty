11c5c35821b2f82f0cec17ec4e97e8162f480e2a
Shubham Singodiya <93720428+MisterMickey@users.noreply.github.com>
2023-06-28 11:24:18 +0530

    Fix spacing in footer.

--- includes/footer.php
+++ includes/footer.php
@@ -33,8 +33,8 @@
     <div class="container mt-4">
         <div class="copyright">
             &copy; Copyright <strong>
-                <script>document.write(new Date().getFullYear());</script><span> Shorty</span>
-            </strong>. All Rights Reserved
+                <script>document.write(new Date().getFullYear());</script><span> Shorty. </span>
+            </strong>All Rights Reserved
         </div>
         <div class="credits">
             Designed by <a target="_blank" href="https://github.com/apoorvaron">Apoorv Aron</a>
