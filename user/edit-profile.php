b6103e4d63e1c2f05dc09d7d6c2f3656c39b2426
apoorvaron <apoorv.aron02@gmail.com>
2023-08-02 18:38:21 +0530

    make email non editable in profile page

--- user/edit-profile.php
+++ user/edit-profile.php
@@ -205,7 +205,7 @@ if (isset($_POST['submit'])) {
                                                                 <label for='example-text-input' class='col-sm-2 col-form-label'>Email</label>
                 
                                                                 <div class='col-sm-10'>
-                                                                    <input class='form-control' type='text' value='" . $row['email'] . "' id='email' name='email' required >
+                                                                    <input class='form-control' readonly type='text' value='" . $row['email'] . "' id='email' name='email' required >
                                                                 </div>
                                                             </div>
                                                             <div class='form-group row'>
