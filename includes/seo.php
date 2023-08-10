92d66c7dcd3dc874dbd94d343a8fa70efb88ca33
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2023-08-10 10:49:54 +0530

    feat: implement browser bar theme feature for shorty

--- includes/seo.php
+++ includes/seo.php
@@ -9,23 +9,40 @@ include(__DIR__ . '/../env.php');
 <meta name="revisit-after" content="7 days" />
 <meta name="distribution" content="global" />
 <meta name="geo.region" content="india" />
-<meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+<meta
+  name="googlebot"
+  content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+/>
 <meta http-equiv="content-language" content="en" />
-<meta name="description"
-  content="Shorty is a powerful URL shortener that allows you to personalize, share, and track your content links, capturing valuable data with every click" />
-<meta name="keywords"
-  content="URL shortener, personalized links, shareable links, trackable links, link management,    Customized links,    Click data,    Link optimization,Link branding,    URL redirection,    Link performance,Link metrics,    URL management" />
+<meta
+  name="description"
+  content="Shorty is a powerful URL shortener that allows you to personalize, share, and track your content links, capturing valuable data with every click"
+/>
+<meta
+  name="keywords"
+  content="URL shortener, personalized links, shareable links, trackable links, link management,    Customized links,    Click data,    Link optimization,Link branding,    URL redirection,    Link performance,Link metrics,    URL management"
+/>
 
 <!-- LINKS SEO (mrprayag077) -->
 <link rel="canonical" href="<?php echo $env_domain ?>" />
-<meta name="og:description"
-  content="Shorty is a powerful URL shortener that allows you to personalize, share, and track your content links, capturing valuable data with every click" />
+<meta
+  name="og:description"
+  content="Shorty is a powerful URL shortener that allows you to personalize, share, and track your content links, capturing valuable data with every click"
+/>
 
 <!-- OPENGRAPH META (0xabdulkhalid) -->
-<meta property="og:title" content="Shorty | Powerful URL Shortener for Personalized, Shareable, and Trackable Links" />
-<meta property="og:description"
-  content="Shorty is a powerful URL shortener that allows you to personalize, share, and track your content links, capturing valuable data with every click" />
-<meta property="og:image" content="<?php echo $env_domain ?>assets/img/inside-header-logo.png" />
+<meta
+  property="og:title"
+  content="Shorty | Powerful URL Shortener for Personalized, Shareable, and Trackable Links"
+/>
+<meta
+  property="og:description"
+  content="Shorty is a powerful URL shortener that allows you to personalize, share, and track your content links, capturing valuable data with every click"
+/>
+<meta
+  property="og:image"
+  content="<?php echo $env_domain ?>assets/img/inside-header-logo.png"
+/>
 <meta property="og:url" content="<?php echo $env_domain ?>" />
 <meta property="og:type" content="website" />
 <meta property="og:site_name" content="Shorty" />
@@ -35,12 +52,28 @@ include(__DIR__ . '/../env.php');
 <meta property="og:website:author" content="Apoorv Aron" />
 
 <!-- TWITTER SEO (0xabdulkhalid) -->
-<meta name="twitter:card" content="<?php echo $env_domain ?>assets/img/inside-header-logo.webp />
+<meta
+  name="twitter:card"
+  content="<?php echo $env_domain ?>assets/img/inside-header-logo.webp>"
+/>
 <meta name="twitter:site" content="@AronApoorv" />
-<meta name="twitter:image" content="<?php echo $env_domain ?>assets/img/inside-header-logo.webp" />
+<meta
+  name="twitter:image"
+  content="<?php echo $env_domain ?>assets/img/inside-header-logo.webp>"
+/>
+
+<!-- BROWSER BAR THEME -->
+<meta name="theme-color" content="#0D42FE" />
+<meta name="msapplication-navbutton-color" content="#151515" />
+<meta name="msapplication-TileColor" content="#0D42FE" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="#0D42FE" />
 
 <!-- <meta content="" name="" /> -->
-<meta content="URL shortener, personalized links, trackable links" name="keywords" />
+<meta
+  content="URL shortener, personalized links, trackable links"
+  name="keywords"
+/>
 
 <!-- Favicons -->
 <link href="./assets/img/logo.webp" rel="icon" />
